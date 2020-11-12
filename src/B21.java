f0e47d0f80fb07c9124735960058cdd988242e44
PhKhrystyna <pihurchristina@gmail.com>
2020-11-12 11:17:29 -0500

    2

---
+++ src/B21.java
@@ -0,0 +1,7 @@
+public class B21 {
+    public static void main(String[] args) {
+        //B-21 is dedicated!
+        //B-21 is successful testers
+        //great day
+    }
+}
---
+++ src/ghg.java
@@ -0,0 +1,7 @@
+public class ghg {
+    public static void main(String[] args) {
+        //dsgsd
+
+           }
+
+}
