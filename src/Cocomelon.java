f0a4589e33bb5f43047d7a022064aad180459d33
PhKhrystyna <pihurchristina@gmail.com>
2020-11-12 11:21:36 -0500

    new file was created

---
+++ src/Cocomelon.java
@@ -0,0 +1,7 @@
+public class Cocomelon {
+    public static void main(String[] args) {
+
+        System.out.println("I don't know what is cocomelon");
+        // cocomelon sounds good tho
+    }
+}
